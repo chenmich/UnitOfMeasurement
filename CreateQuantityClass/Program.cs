b5bca86a199ed8cace57382e20b57848a5506792
chenmich <403189929@qq.com>
2018-06-19 16:15:21 +0800

    modify the Program file

--- CreateQuantityClass/Program.cs
+++ CreateQuantityClass/Program.cs
@@ -15,42 +15,7 @@ namespace CreateQuantityClass
             DirectoryInfo groupDir = new DirectoryInfo(currentdir);
             string templategroupfile = groupDir.FullName + "/UOM.stg";
             SIQTSet siqts = new SIQTSet(templategroupfile);
-            Console.WriteLine(siqts.getQTs());
-            
-        }
-
-        static List<QT> getQTs(){
-            string[] typeName = {
-                "Identity", "Length", "Mass", 
-                "Time", "ElectricCurrent", "ThermodynamicTemperature", 
-                "AmountOfSubstance", "LuminousIntensity"
-            };
-            string[] typeName_by_base = {"Velocity", 
-                "Acceleration", "Area", "Volumn", 
-                "Force", "Stress"
-            };
-
-            QT[] qtps = new QT[]{
-                new QT(){qtCode="1", qtName="Identity", qtId=Guid.NewGuid(), 
-                            qtExp=new QTExpression(){leftCode=null, rightCode=null, op=null}},
-
-                new QT(){qtCode="2", qtName="Length", qtId=Guid.NewGuid(), 
-                            qtExp=new QTExpression(){leftCode=null, rightCode=null, op=null}},
-
-                new QT(){qtCode="3", qtName="Mass", qtId=Guid.NewGuid(), 
-                            qtExp=new QTExpression(){leftCode=null, rightCode=null, op=null}},
-                new QT(){qtCode="4", qtName="Time", qtId=Guid.NewGuid(),
-                            qtExp=new QTExpression(){leftCode=null, rightCode=null, op=null}},
-                            
-                new QT(){qtCode="100", qtName="Velocity", qtId=Guid.NewGuid(), 
-                            qtExp=new QTExpression(){leftCode="2", rightCode="4", op="Multiply"}}
-            };
-
-            List<QT> qts = new List<QT>();
-
-            foreach(QT qt in qtps){qts.Add(qt);}
-           
-            return qts;
+            Console.WriteLine(siqts.getQTs());            
         }
     }
 }
